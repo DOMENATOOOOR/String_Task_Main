2e2bb9438507b1710ee69b21617096f5a3079216
Pan_Sasha <pankov.aleksander05@gmail.com>
2025-03-30 13:17:56 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,26 +1,29 @@
 #include "String.h"
 
+// разнести по файлам
 int main() {
     String str1("Hello");
     String str2("dddd");
     String str3("World");
     str3[2] = 'F';
+    std::cout << str3.size() << std::endl;
     str3.erase(1, 3);
     str3.insert(2, str1);
     str1 += "str987654321";
     str1.replace(5,3,4,'d');
-    size_t n1 = str1.find(str3, 0);
-    size_t n2 = str1.find('s', 0);
-    size_t n3 = str1.find("srew", 0);
+    std::cout<<str1.find(str3, 0)<<std::endl;;
+    std::cout<<str1.find('s', 0)<<std::endl;
+    std::cout<<str1.find("srew", 0)<<std::endl;;
+    String str6 = str1;
+    str1.replace(5,3,4,'d');
     str1.swap(str2);
+    std::cout<<str6.compare(str2)<< std::endl;
     String str4 = str1;
+    str4.reserve(1);
     std::cout << str4[2] << std::endl;
-    String str5;
-    str5 = str4.substr(2, 2);
+    String str5 = str4.substr(2, 2);
     std::cout<<str2.data()<< std::endl;
-    str1.clear();
-    str2.clear();
-    str3.clear();
-    str4.clear();
+    str2.insert(2, "What the fox say?");
+    std::cout<<str2.data()<< std::endl;
     return 0;
 }
