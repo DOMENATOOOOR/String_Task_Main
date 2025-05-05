9c9646210ed05bce85265bd3c28bfcca707e16c3
Pan_Sasha <pankov.aleksander05@gmail.com>
2025-05-05 16:56:30 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,10 +1,10 @@
 #include "String.h"
 
-// разнести по файлам
 int main() {
     String str1("Hello");
     String str2("dddd");
     String str3("World");
+    std::cout<<str3.at(1)<<std::endl;
     str3[2] = 'F';
     std::cout << str3.size() << std::endl;
     str3.erase(1, 3);
@@ -15,15 +15,22 @@ int main() {
     std::cout<<str1.find('s', 0)<<std::endl;
     std::cout<<str1.find("srew", 0)<<std::endl;;
     String str6 = str1;
+    str6.at(1) = str1.at(3);
+    str6.erase();
     str1.replace(5,3,4,'d');
     str1.swap(str2);
     std::cout<<str6.compare(str2)<< std::endl;
-    String str4 = str1;
+    String str4(str1);
     str4.reserve(1);
+    str4 += str1;
+    str4 += "fsss";
     std::cout << str4[2] << std::endl;
     String str5 = str4.substr(2, 2);
     std::cout<<str2.data()<< std::endl;
     str2.insert(2, "What the fox say?");
+    str2.clear();
     std::cout<<str2.data()<< std::endl;
+    str5.clear();
+    str4.clear();
     return 0;
 }
